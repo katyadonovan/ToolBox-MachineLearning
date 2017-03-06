2d27545215817b2f7505479f1004a8d25128c11f
Katya <katya@computer.com>
2017-03-05 21:11:04 -0500

    Machine Learning Tool Box!

--- learning_curve.py
+++ learning_curve.py
@@ -14,33 +14,38 @@ def display_digits():
     for i in range(10):
         subplot = fig.add_subplot(5, 2, i+1)
         subplot.matshow(numpy.reshape(digits.data[i], (8, 8)), cmap='gray')
-
     plt.show()
-
 
 def train_model():
     data = load_digits()
     num_trials = 10
     train_percentages = range(5, 95, 5)
-    test_accuracies = numpy.zeros(len(train_percentages))
-
-    # train models with training percentages between 5 and 90 (see
-    # train_percentages) and evaluate the resultant accuracy for each.
-    # You should repeat each training percentage num_trials times to smooth out
-    # variability.
-    # For consistency with the previous example use
-    # model = LogisticRegression(C=10**-10) for your learner
-
-    # TODO: your code here
-
-    fig = plt.figure()
+    test_accuracies =[] #numpy.zeros(len(train_percentages))
+    for i in train_percentages:
+        total_accuracy = []
+        for j in range(num_trials):
+            X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size=i/100)
+            model = LogisticRegression(C=10**-10)
+            model.fit(X_train, y_train)
+            total_accuracy.append(model.score(X_test, y_test))
+        avg = sum(total_accuracy)/len(total_accuracy)
+        test_accuracies.append(avg)
+        # train models with training percentages between 5 and 90 (see
+        # train_percentages) and evaluate the resultant accuracy for each.
+        # You should repeat each training percentage num_trials times to smooth out
+        # variability.
+        # For consistency with the previous example use
+        # model = LogisticRegression(C=10**-10) for your learner
+    #print(len(test_accuracies))
+    fig1 = plt.figure()
     plt.plot(train_percentages, test_accuracies)
     plt.xlabel('Percentage of Data Used for Training')
     plt.ylabel('Accuracy on Test Set')
     plt.show()
 
 
+
 if __name__ == "__main__":
     # Feel free to comment/uncomment as needed
-    display_digits()
-    # train_model()
+    #display_digits()
+    train_model()
